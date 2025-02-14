03696f5d6134b79aad90d7b881a40fd3a90ed012
Rego08 <regomark866@gmail.com>
2025-02-14 09:05:32 +0200

    Add files via upload

---
+++ H6.ps1
@@ -0,0 +1,29 @@
+﻿$a = "Tekst"
+$b = 3
+$c = 3.14
+[datetime] - kuupäev või kellaaeg
+[string] - sõne
+[char] - üksik märk
+[double] - komaarvud
+[int] - täisarv
+[wmi] - Windows Management Instrumentation (WMI) objekt
+[adsi] - Active Directory Services objekt
+[wmiclass] - WMI klass
+[Boolean] - tõene või väär
+$teenus[2,4,5]
+$teenus[-1]
+$serverid = @{
+ "server1" = "192.168.1.30";
+ "server2" = "192.168.1.40";
+ "server3" = "192.168.1.50"
+ }
+  $serverid.keys
+ $serverid.values
+ $domeen = "metshein.com"
+$domeen[0]
+$uusDomeen = $domeen.ToCharArray()
+$nimed = "Rheta,Bryant,Norby,Geoff,Jarret,Eleonora,Everard,Charin,Nils,Chaddy"
+$nimed.split(",")
+$nimed = @("Rheta","Bryant","Norby","Geoff","Jarret","Eleonora","Everard","Charin","Nils")
+$nimed -join ","
+#väljund: Rheta,Bryant,Norby,Geoff,Jarret,Eleonora,Everard,Charin,Nils
