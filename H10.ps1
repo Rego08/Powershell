fda82699f701b57af668cc9d2c3fdfe9ea91f2f4
Rego08 <regomark866@gmail.com>
2025-02-14 09:18:12 +0200

    Add files via upload

---
+++ H10.ps1
@@ -0,0 +1,27 @@
+﻿gci c:\Windows\ | Export-Clixml  c:\windows.xml
+Import-Clixml  c:\windows.xml
+<?xml version='1.0' encoding='UTF-8'?>
+<autod>
+	<auto>
+		<automark>Hyundai</automark>
+		<mudel>Elantra</mudel>
+		<aasta>1993</aasta>
+	</auto>
+	<auto>
+		<automark>Mazda</automark>
+		<mudel>MX-5</mudel>
+		<aasta>1994</aasta>
+	</auto>
+</autod>
+$xml = [xml](Get-Content c:\autod.xml)
+$xml.autod.auto
+$xml = [xml](Get-Content c:\autod.xml)
+$xml.autod.auto.automark
+$xml = [xml](Get-Content c:\autod.xml)
+$rows = $xml.autod.auto
+foreach ($row in $rows)
+{
+    if($row.aasta -gt 2000){
+        $row.automark
+    }
+}
